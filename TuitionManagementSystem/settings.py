a85821747a2f98c3d8bc757d80a0e3354532ea4a
Sridhar <srid20ad047@rmkcet.ac.in>
2023-03-13 21:24:22 +0530

    Attendance Page Updated

--- TuitionManagementSystem/settings.py
+++ TuitionManagementSystem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-np=37ta&&*uqvu@skp@y(n#1n4pfq(n@c_4kwj0xrf32pie$1c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
